e600287c77cc9ae1153ccb168a7e6d6f8422b14e
natsamjosh@excite.com <natsamjosh@excite.com>
2019-10-29 19:02:55 -0400

    First real commit using github

--- basicCar.py
+++ basicCar.py
@@ -6,6 +6,12 @@ class Car:
     def accelerate(self, footPressure):
         return (footPressure * self.engine)
 
+class SUV(Car):
+    def calculateCargo(self, l, w, h):
+        print("Hello")
+        self.space = l * w * h
+        return self.space
+
 def ipValidate(ipAddr):
     ipOctets = ipAddr.split(".")
     if len(ipOctets) != 4:
@@ -20,6 +26,12 @@ car1 = Car("Chevy", 454)
 pressure = 12             #psi
 acceleration = car1.accelerate(pressure)
 print (str(acceleration))
+
+suv2 = SUV("Chevy", 454)
+l = 5
+w = 4
+h = 3
+print(str(suv2.calculateCargo(l, w, h)))
 
 if ipValidate("2.128.90.344"):
     print("Ip address is valid")
